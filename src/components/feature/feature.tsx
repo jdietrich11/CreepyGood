a933909db3e8db67fd1f9876a7672f2ce81197c7
josh dietrich <jbdietrich2@gmail.com>
2023-07-12 05:45:09 -0600

    added shop now btn

--- src/components/feature/feature.tsx
+++ src/components/feature/feature.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
+import ShopNowBtn from "../shopNowBtn/shopNowBtn";
 
 import "./featureStyles.scss";
 
@@ -32,6 +34,7 @@ const FeaturedProduct: React.FC = () => {
               onClick={() => setFeatureColor("Black")}
             ></div>
           </div>
+          <ShopNowBtn />
         </div>
       </div>
     </div>
---
+++ src/components/shopNowBtn/shopNowBtn.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./shopNowBtnStyles.scss";
+
+const ShopNowBtn: React.FC = () => {
+  return (
+    <div className="shopNowContainer">
+      <div className="shopNowText">SHOP NOW!</div>
+    </div>
+  );
+};
+
+export default ShopNowBtn;
