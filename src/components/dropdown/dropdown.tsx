aeb0f3b44627e675810fb9d5d04ab293fac4ac31
joshua <jbdietrich2@gmail.com>
2023-07-12 19:30:43 -0600

    added dropdown

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Icon from "../../helper/icon/icon";
+
+import "./dropdown.styles.scss";
 
 interface DropdonwProps {
   name: string;
@@ -6,11 +10,29 @@ interface DropdonwProps {
 }
 
 const Dropdown: React.FC<DropdonwProps> = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
   const { name, options } = props;
 
   return (
-    <div>
-      <div>{name}</div>
+    <div className="dropdownContainer">
+      <div className="dropdownNameContainer" onClick={() => setIsOpen(!isOpen)}>
+        <div className="dropdownName">{name}</div>
+        <Icon
+          icon="cheveron-down"
+          size="3rem"
+          color="white"
+          className={isOpen ? "icon flip" : "icon"}
+        />
+      </div>
+      <div className={isOpen ? "optionsContainer" : "none"}>
+        {isOpen ? (
+          options.map((item) => {
+            return <div className="option">{item}</div>;
+          })
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -10,39 +10,40 @@ import "./footer.styles.scss";
 const Footer: React.FC = () => {
   return (
     <div className="footerContainer">
-      <div className="iconContainer">
-        <div className="followText">Follow us</div>
-        <a href="http://facebook.com/CreepyGood">
-          <Icon icon="facebook2" size="3rem" color="white" className="icon" />
-        </a>
-        <a href="http://instagram.com/Creepy_Good">
-          <Icon icon="instagram" size="3rem" color="white" className="icon" />
-        </a>
-      </div>
-      {footerOptions.map((item) => {
-        return <Dropdown name={item.name} options={item.options} />;
-      })}
-      <div className="textContainer">
-        <div>
-          Powered by{" "}
-          <a
-            className="link"
-            href="https://www.threadless.com/artist-shops/signup?thref=shop_creepygood&utm_medium=artist_shop&utm_source=creepygood&utm_campaign=powered_by"
-          >
-            Threadless Artist Shops
+      <div className="footerContainerImage">
+        <div className="iconContainer">
+          <div className="followText">Follow us</div>
+          <a href="http://facebook.com/CreepyGood">
+            <Icon icon="facebook2" size="3rem" color="white" className="icon" />
+          </a>
+          <a href="http://instagram.com/Creepy_Good">
+            <Icon icon="instagram" size="3rem" color="white" className="icon" />
           </a>
         </div>
-        <div>
-          &copy;CreepyGood's Artist Shop /{" "}
-          <a className="link" href="#">
-            Help & Returns
-          </a>{" "}
-          /{" "}
-          <a className="link" href="#">
-            Legal
-          </a>
+        {footerOptions.map((item) => {
+          return <Dropdown name={item.name} options={item.options} />;
+        })}
+        <div className="textContainer">
+          <div>
+            Powered by{" "}
+            <a
+              className="link"
+              href="https://www.threadless.com/artist-shops/signup?thref=shop_creepygood&utm_medium=artist_shop&utm_source=creepygood&utm_campaign=powered_by"
+            >
+              Threadless Artist Shops
+            </a>
+          </div>
+          <div>
+            &copy;CreepyGood's Artist Shop /{" "}
+            <a className="link" href="#">
+              Help & Returns
+            </a>{" "}
+            /{" "}
+            <a className="link" href="#">
+              Legal
+            </a>
+          </div>
         </div>
-        D
       </div>
     </div>
   );
